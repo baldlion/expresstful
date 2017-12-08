cca4d65a1fe0b0ad88e03ed1a15c7e2bd80c0135
Dustin Krotts <dkrotts@Dustins-MacBook-Pro.local>
2017-12-08 13:26:24 -0500

    0.0.6

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
-export { expresstful as default } from './expresstful'
-export { controller, all, del, get, post, put } from './decorators'
+import expresstful from './expresstful'
+import { controller, all, del, get, post, put } from './decorators'
+
+export {
+  expresstful,
+  controller,
+  all,
+  del,
+  get,
+  post,
+  put
+}
